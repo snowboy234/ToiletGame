758033709f7065a0af2aad06f4ce9e147b323794
tianwei <tianwei223344@163.com>
2017-08-21 09:54:11 +0800

    初始化项目

---
+++ ToiletGame/ToiletGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToiletGame
+//
+//  Created by 田伟 on 2017/8/21.
+//  Copyright © 2017年 田伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ToiletGame/ToiletGame/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ToiletGame
+//
+//  Created by 田伟 on 2017/8/21.
+//  Copyright © 2017年 田伟. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
